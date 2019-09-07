f5f313120f35cd98db0881c915d1a0a80d423413
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-09-07 02:58:20 -0300

    Removing products from cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,4 +1,4 @@
-import producer from 'immer';
+import producer, { produce } from 'immer';
 
 const INITIAL_STATE = {
   product: [],
@@ -19,6 +19,15 @@ export default function cart(state = [], action) {
           });
         }
       });
+    case 'REMOVE_FROM_CART': {
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+    }
     default:
       return state;
   }
