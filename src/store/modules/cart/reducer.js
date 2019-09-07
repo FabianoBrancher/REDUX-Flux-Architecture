e8988fb8e7b7c8c95d049b125f4db4f8b5f9a6aa
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-09-07 02:18:11 -0300

    Adding to cart, connecting to the store

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,13 @@
-export default function cart() {
-  return [];
+const INITIAL_STATE = {
+  product: null,
+};
+
+export default function cart(state = [], action) {
+  console.log(state);
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, action.product];
+    default:
+      return state;
+  }
 }
