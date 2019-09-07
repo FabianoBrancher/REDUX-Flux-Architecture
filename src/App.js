c4317b82c06b542231fc1888d3b9740329a0cdc5
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-09-07 00:05:32 -0300

    Added routes, global styles, Home and Cart pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+import GlobalStyle from '~/styles/global';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
