2659fbbb46d8ad11dfd86bf105b8b9ce61ace1e5
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-09-07 02:53:42 -0300

    Handling products duplicity

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,17 +1,24 @@
+import producer from 'immer';
+
 const INITIAL_STATE = {
-  product: null,
+  product: [],
 };
 
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return producer(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
