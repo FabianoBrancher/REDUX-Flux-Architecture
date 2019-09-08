27a2f7e2cd6c3ff30f1168c1c83aab0368610818
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-09-08 01:53:45 -0300

    Configured Redux Saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '~/store/modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMidlleware = createSagaMiddleware();
 
 const enhancer =
   process.env.NODE_ENV === 'development'
-    ? console.tron.createEnhancer()
-    : null();
+    ? compose(
+        console.tron.createEnhancer(),
+        applyMiddleware(sagaMidlleware)
+      )
+    : applyMiddleware(sagaMidlleware);
 
 const store = createStore(rootReducer, enhancer);
+
+sagaMidlleware.run(rootSaga);
 
 export default store;
